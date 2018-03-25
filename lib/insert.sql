e710ddfae939417412cbf9581248d4027d90061d
Denise Pendleton <dpendl1@hotmail.com>
2018-03-25 21:31:27 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive)
     VALUES ("Mr. Chocolate", 3, m, "brown", "happy", f );
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-        VALUES ("Rowdy", );
+        VALUES ("Rowdy", 15, m, "brown", "sad", t );
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-            VALUES ("Tabitha",2 );
+            VALUES ("Tabitha", 7, f, "black", "happy", t );
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-                VALUES ("Sergeant Brown", );
+                VALUES ("Sergeant Brown", 20, f, "black", "angry", t  );
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-                    VALUES ("Melissa", );
+                    VALUES ("Melissa", 13, m, "brown", "happy", f );
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-                        VALUES ("Grinch", );
+                        VALUES ("Grinch", 23, f, "black", "overit", f );
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-                            VALUES ("Wendy", );
+                            VALUES ("Wendy", 29, m, "brown", "giddy", f );
 INSERT INTO bears (name, age, gender, color, temperament, alive)
-                                VALUES (null, );
+                                VALUES (null, null, m, "brown", "sad", f );
