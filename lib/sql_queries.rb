331e8dafb31259339a70a8a9a855a43f39ec614f
Denise Pendleton <dpendl1@hotmail.com>
2018-03-25 22:45:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   # "SELECT color, COUNT(color) FROM bears ORDER BY color DESC LIMIT 1;"
-  "select color, count(*) from bears ORDER BY color DESC LIMIT 1"
+  "select color, count(DISTINCT color) from bears GROUP BY color ;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
